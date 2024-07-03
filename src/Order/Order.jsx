fa50cfd16ac7027ce12a06f8921a48ba451f14dd
Aman Rajput <rajputaman724857@gmail.com>
2024-07-03 21:46:10 +0530

    resolve error

--- src/Order/Order.jsx
+++ src/Order/Order.jsx
@@ -42,16 +42,16 @@ useEffect(()=>{
           console.log("data on map", orderData);
 
             // Extracting quantities from orderquantity
-            const quantities = orderData?.orderquantity.map(item => item.quantity);
+            const quantities = orderData?.orderquantity?.map(item => item?.quantity);
             console.log("quantities", quantities);
           return (
             <div className='border border-slate-950 m-4 p-1 rounded-md' key={orderData._id}>
-              <h1 className='font-bold ml-4 text-2xl'>TotalBill ₹ {orderData.BillAmount}</h1>
+              <h1 className='font-bold ml-4 text-2xl'>TotalBill ₹ {orderData?.BillAmount}</h1>
               <h2 className=' ml-4 '>BillStatus <span className='font-thin'> {orderData?.BillStatus}
                
               <p> Order Time {new Date(orderData?.createdAt).toLocaleString()} </p>
               <p>Delivery Address <span className='font-semibold'>{orderData?.address?.street+ " "+orderData?.address?.city +" "+ orderData?.address?.state}</span></p>
-              <p> PostalCode {orderData.address.postalcode}</p>
+              <p> PostalCode {orderData?.address?.postalcode}</p>
 
               </span>
               </h2>
@@ -60,9 +60,9 @@ useEffect(()=>{
                 orderData?.products?.map((order,i) => (
                   <div className='flex flex-col  items-center lg:flex-row lg:justify-around   mt-10' key={order._id}>
                     <div className='w-1/2'>
-                      <h2>{order.title}</h2>
-                      <p ><span className='font-bold line-through'> ₹ {order.price} </span>{order.discountPercentage} % off</p>
-                           <p>₹ {Math.round(order.price-(order.discountPercentage*order.price)/100)}</p>
+                      <h2>{order?.title}</h2>
+                      <p ><span className='font-bold line-through'> ₹ {order?.price} </span>{order?.discountPercentage} % off</p>
+                           <p>₹ {Math.round(order?.price-(order?.discountPercentage*order?.price)/100)}</p>
                            <p className='font-bold'>Quantity {quantities[i]}</p>
                     </div>
                     <div className='w-1/4 mt-2'>
