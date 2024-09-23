2f58679a84dab1178dc1b06d59789d4b65f11a19
Aman Rajput <rajputaman724857@gmail.com>
2024-09-23 14:12:07 +0530

    url change of payment

--- Hooks/UsePayment.js
+++ Hooks/UsePayment.js
@@ -26,7 +26,7 @@ const Payment = async(orderdata,cart,cartBalance,user,token,toast,dispatch,navig
          if(!(orderdata.paymentMethod==="Cash")){
           console.log("online payment",orderdata.paymentMethod,cartBalance.totalAmount,cartBalance.totalquantity);
           try {
-            const orderResponse = await axios.post("https://mern-ecomapp-1.onrender.com/v1/payment/checkout",{
+            const orderResponse = await axios.post("https://ecomapp-5huy.onrender.com/v1/payment/checkout",{
               amount: cartBalance.totalAmount,
               cartItems: cartBalance.totalquantity,
               userShipping:orderdata.address,
